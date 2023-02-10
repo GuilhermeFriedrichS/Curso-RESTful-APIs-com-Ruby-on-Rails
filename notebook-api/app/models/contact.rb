78eb729cf0ac49dce0647804daaf24c64502b980
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-02-10 09:17:14 -0300

    Add field in permit list

--- notebook-api/app/models/contact.rb
+++ notebook-api/app/models/contact.rb
@@ -1,19 +1,19 @@
 class Contact < ApplicationRecord
-    belongs_to :kind
+    belongs_to :kind#, optional: true deixa o usario cadastrar contato sem o tipo
 
-    def author
-        "Jackson Pires"
-    end
+    #def author
+    #    "Jackson Pires"
+    #end
 
-    def kind_description
-        self.kind.description
-    end
+    #def kind_description
+    #    self.kind.description
+    #end
 
-    def as_json(options={}) #Esse metodo é chamado sozinho quando é gerado uma resposta JSON
-        super(
-            root: true,
-            methods: [:kind_description, :author] #Chama o metodo que adiciona a descrição do Kind sem criar os {} na resposta do JSON
-            #include: { kind: { only: :description}} # Adiciona descrição na resposta JSON, mas cria os {}
-        )
-    end
+    #def as_json(options={}) #Esse metodo é chamado sozinho quando é gerado uma resposta JSON
+    #    super(
+    #        root: true,
+    #        methods: [:kind_description, :author] #Chama o metodo que adiciona a descrição do Kind sem criar os {} na resposta do JSON
+    #        #include: { kind: { only: :description}} # Adiciona descrição na resposta JSON, mas cria os {}
+    #    )
+    #end
 end

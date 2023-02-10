05116490c8a9072b87a2de70191a2efa50371289
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-02-10 08:55:45 -0300

    Change method to add fields in JSON response

--- notebook-api/app/models/contact.rb
+++ notebook-api/app/models/contact.rb
@@ -5,7 +5,15 @@ class Contact < ApplicationRecord
         "Jackson Pires"
     end
 
-    def as_json(options={})
-        super(methods: :author, root: true)
+    def kind_description
+        self.kind.description
+    end
+
+    def as_json(options={}) #Esse metodo é chamado sozinho quando é gerado uma resposta JSON
+        super(
+            root: true,
+            methods: [:kind_description, :author] #Chama o metodo que adiciona a descrição do Kind sem criar os {} na resposta do JSON
+            #include: { kind: { only: :description}} # Adiciona descrição na resposta JSON, mas cria os {}
+        )
     end
 end

1bb877daafcf3bfbb80e53949df2e8cd9af6844c
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-02-14 08:29:30 -0300

    Refact routes and create controllers to kids and phones

--- notebook-api/config/routes.rb
+++ notebook-api/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :kinds
-  resources :contacts
+  resources :contacts do 
+    resource :kind, only: [:show] #Cria a rota que retona o kind do contato sem precisar passar o id do kind (belongs_to)
+    resource :kind, only: [:show], path: 'relationships/kind'
+
+    resource :phones, only: [:show] 
+    resource :phones, only: [:show], path: 'relationships/phones'
+    
+    resource :address, only: [:show] 
+    resource :address, only: [:show], path: 'relationships/address'
+  end
   #get '/contacts', to: "contacts#index" Rota manual
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

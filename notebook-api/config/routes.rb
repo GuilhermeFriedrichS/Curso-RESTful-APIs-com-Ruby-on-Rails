7d288aad11d6cf7fb5b1e908905b285803b9fb76
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-02-15 09:53:12 -0300

    Change versioning to work with URL

--- notebook-api/config/routes.rb
+++ notebook-api/config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
 
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :auths, only: [:create]
+  # resources :auths, only: [:create]
   resources :kinds
 
-  api_version(:module => "V1", :parameter => {:name => "version", :value => "1"}) do
+  api_version(:module => "V1", :path => {:value => "v1"}) do
     resources :contacts do
       resource :kind, only: [:show] #Cria a rota que retona o kind do contato sem precisar passar o id do kind (belongs_to)
       resource :kind, only: [:show], path: 'relationships/kind'
 
       resource :phones, only: [:show] 
       resource :phones, only: [:show], path: 'relationships/phones'
+      
       resource :phone, only: [:show, :update, :create, :destroy] 
       resource :phone, only: [:show, :update, :create, :destroy], path: 'relationships/phone'
       
@@ -19,19 +20,20 @@ Rails.application.routes.draw do
     end
   end
   
-  api_version(:module => "V2", :parameter => {:name => "version", :value => "2"}) do
-    resources :contacts do 
-      resource :kind, only: [:show] #Cria a rota que retona o kind do contato sem precisar passar o id do kind (belongs_to)
-      resource :kind, only: [:show], path: 'relationships/kind'
+  api_version(:module => "V2", :path => {:value => "v2"}) do
+      resources :contacts do
+        resource :kind, only: [:show] #Cria a rota que retona o kind do contato sem precisar passar o id do kind (belongs_to)
+        resource :kind, only: [:show], path: 'relationships/kind'
 
-      resource :phones, only: [:show] 
-      resource :phones, only: [:show], path: 'relationships/phones'
-      resource :phone, only: [:show, :update, :create, :destroy] 
-      resource :phone, only: [:show, :update, :create, :destroy], path: 'relationships/phone'
-      
-      resource :address, only: [:show, :update, :create, :destroy]
-      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
-    end
+        resource :phones, only: [:show] 
+        resource :phones, only: [:show], path: 'relationships/phones'
+        
+        resource :phone, only: [:show, :update, :create, :destroy] 
+        resource :phone, only: [:show, :update, :create, :destroy], path: 'relationships/phone'
+        
+        resource :address, only: [:show, :update, :create, :destroy]
+        resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+      end
   end
 
 end

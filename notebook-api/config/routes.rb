7dde95c752d69823f25148e444583922ac3f9c4b
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-02-15 08:40:14 -0300

    Create versioning with URL params

--- notebook-api/config/routes.rb
+++ notebook-api/config/routes.rb
@@ -1,19 +1,37 @@
 Rails.application.routes.draw do
+
   mount_devise_token_auth_for 'User', at: 'auth'
   resources :auths, only: [:create]
   resources :kinds
-  resources :contacts do 
-    resource :kind, only: [:show] #Cria a rota que retona o kind do contato sem precisar passar o id do kind (belongs_to)
-    resource :kind, only: [:show], path: 'relationships/kind'
 
-    resource :phones, only: [:show] 
-    resource :phones, only: [:show], path: 'relationships/phones'
-    resource :phone, only: [:show, :update, :create, :destroy] 
-    resource :phone, only: [:show, :update, :create, :destroy], path: 'relationships/phone'
-    
-    resource :address, only: [:show, :update, :create, :destroy]
-    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+  scope module: 'v1' do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == "1" } do 
+      resource :kind, only: [:show] #Cria a rota que retona o kind do contato sem precisar passar o id do kind (belongs_to)
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      resource :phones, only: [:show] 
+      resource :phones, only: [:show], path: 'relationships/phones'
+      resource :phone, only: [:show, :update, :create, :destroy] 
+      resource :phone, only: [:show, :update, :create, :destroy], path: 'relationships/phone'
+      
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
   end
-  #get '/contacts', to: "contacts#index" Rota manual
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: 'v2' do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == "2" } do 
+      resource :kind, only: [:show] #Cria a rota que retona o kind do contato sem precisar passar o id do kind (belongs_to)
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      resource :phones, only: [:show] 
+      resource :phones, only: [:show], path: 'relationships/phones'
+      resource :phone, only: [:show, :update, :create, :destroy] 
+      resource :phone, only: [:show, :update, :create, :destroy], path: 'relationships/phone'
+      
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
+  end
+
 end
